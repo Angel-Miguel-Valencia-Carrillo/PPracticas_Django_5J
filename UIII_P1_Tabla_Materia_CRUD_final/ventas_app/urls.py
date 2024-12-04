e6675d0eb60943e113ebe9162f1033b0d2126830
Angel-Miguel-Valencia-Carrillo <a.22308051280738@cbtis128.edu.mx>
2024-12-04 10:20:18 -0600

    Delete UIII_P1_Tabla_Materia_CRUD_final directory

--- UIII_P1_Tabla_Materia_CRUD_final/ventas_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from ventas_app import views
-urlpatterns = [
-    path("",views.inicio_vista, name="inicio_vista"),
-    path("registrarVenta/",views.registrarVenta,name="registrarVenta"),
-    path("seleccionarVenta/<id_venta>",views.seleccionarVenta,name="seleccionarVenta"),
-    path("editarVenta/",views.editarVenta,name="editarVenta"),
-    path("borrarVenta/<id_venta>",views.borrarVenta,name="borrarVenta")
-]
